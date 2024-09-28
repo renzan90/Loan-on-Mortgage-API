5e05419bda274939182d8172989b951c6cbc95b8
paritosh <paritosh.jadhav@outlook.com>
2024-09-28 21:20:42 +0530

    added mortgage project to git

---
+++ loans/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+#from views import *
+
+# url_patterns = [
+#     path('', ListRetrieveViewset, name='see-loans'),
+#     path('')
+# ]
---
+++ mortgage/celery.py
@@ -0,0 +1,11 @@
+import os
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mortgage.settings')
+
+app = Celery('mortgage')
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+app.conf.broker_connection_retry_on_startup = True
