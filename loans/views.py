b6513892f553424c7162fde7751c11df8316382e
paritosh <paritosh.jadhav@outlook.com>
2024-09-29 17:38:13 +0530

    created the LoanViewset

--- loans/views.py
+++ loans/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.viewsets import Viewset
+from loans.models import Loans
+from loans.serializer import LoanSerializer
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from django.shortcuts import get_object_or_404
 
-# Create your views here.
+def LoanViewset(viewset):
+
+    def list(self, request, *args, **kwargs):
+        queryset = Loans.objects.all()
+        serialized_data = LoanSerializer(queryset, many=True)
+        return Response(serialized_data.data, status=HTTP_200_OK)
+    
+    def retrieve(self, request, pk):
+        queryset = Loans.objects.all()
+        object = get_object_or_404(queryset, pk)
+        serialized_data = LoanSerializer(object)
+        return Response(serialized_data.data, status=HTTP_200_OK)
+    
+
