41ea7780f87d9e4e812b06e5779a575c94c7f625
paritosh <paritosh.jadhav@outlook.com>
2024-10-02 18:19:53 +0530

    created EligibilityFormSerializer

--- loans/serializers.py
+++ loans/serializers.py
@@ -5,3 +5,9 @@ class LoanSerialzer(serializers.ModelSerializer):
     class Meta:
         model = Loan
         fields = '__all__'
+
+class EligibilityFormSerializer(serializers.Serializer):
+    mortgagable_property = serializers.CharField(max_length=20)
+    property_valuation = serializers.IntegerField()
+    taxable_monthly_income = serializers.IntegerField()
+    
